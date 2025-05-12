13bc6e25bdcb1be3f3044062e798cee877ecd4a1
Otilia Gita <otilgita@emeal.nttdata.com>
2025-05-12 20:42:51 +0300

    initial commit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { TestComponent } from './test/test.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HoursListComponent } from './pages/hours-list/hours-list.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'test', pathMatch: 'full' },
-  { path: 'test', component: TestComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'hours-list', component: HoursListComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: '**', redirectTo: '' }  // redirecționare pentru rute inexistente
 ];
---
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../../app.component';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+
+})
+export class NavbarComponent {
+
+}
