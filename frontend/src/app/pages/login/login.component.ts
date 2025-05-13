7c60ad8d7f832a58057d33866a5d5bd0173b8a4e
Otilia <otilgita@emeal.nttdata.com>
2025-05-13 16:15:19 +0300

    Update login.component.ts

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -28,18 +28,18 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  // Getter pentru acces mai ușor la câmpurile formularului
+
   get f() { return this.loginForm.controls; }
 
   onSubmit() {
     this.submitted = true;
     
-    // Verifică dacă formularul este valid
+
     if (this.loginForm.invalid) {
       return;
     }
     
-    // Verifică dacă email-ul are domeniul @ulbsibiu.com
+
     const email = this.loginForm.value.email;
     if (!email.endsWith('@ulbsibiu.ro')) {
       this.invalidDomain = true;
@@ -48,10 +48,9 @@ export class LoginComponent implements OnInit {
     
     this.invalidDomain = false;
     
-    // TODO: Implementează logica de autentificare
+
     console.log('Formularul a fost trimis:', this.loginForm.value);
     
-    // Redirecționare după autentificare cu succes
-    // this.router.navigate(['/']);
+
   }
 }
